2ee4127f40f2b02d36132e1104284ecf9736d810
Ilya Mirin <ilya.mirin@gmail.com>
2013-10-25 15:04:04 +1100

    make IntegrationTest check chunks in the end

--- src/test/java/me/ilyamirin/anthophila/IntegrationTest.java
+++ src/test/java/me/ilyamirin/anthophila/IntegrationTest.java
@@ -1,5 +1,8 @@
 package me.ilyamirin.anthophila;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import me.ilyamirin.anthophila.client.OneNodeClient;
@@ -14,10 +17,21 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import me.ilyamirin.anthophila.client.ClusterClient;
+import me.ilyamirin.anthophila.common.Node;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -28,8 +42,17 @@ import static org.junit.Assert.assertTrue;
 @Slf4j
 public class IntegrationTest {
 
+    @Data
+    @AllArgsConstructor
+    private class Pair {
+
+        private ByteBuffer key;
+        private ByteBuffer value;
+    }
+
     private Random r = new Random();
 
+    //@Ignore
     @Test
     public void simpleTest() throws IOException, InterruptedException {
         File file = new File("test.bin");
@@ -39,7 +62,7 @@ public class IntegrationTest {
         file.createNewFile();
 
         final String host = "127.0.0.1";
-        final int port = 7621;
+        final int port = 7619;
 
         ServerParams serverParams = new ServerParams();
         serverParams.setStorageFile("test.bin");
@@ -68,8 +91,10 @@ public class IntegrationTest {
         final int requestsNumber = 1000;
         final AtomicInteger errorsCounter = new AtomicInteger(0);
         final AtomicInteger requestsPassed = new AtomicInteger(0);
-        final AtomicInteger chunksStored = new AtomicInteger(0);
+        final AtomicInteger chunksProcessed = new AtomicInteger(0);
         final CountDownLatch latch = new CountDownLatch(clientsNumber);
+        final List<Pair> storedChunks = Collections.synchronizedList(new ArrayList<Pair>());
+        final List<ByteBuffer> removedKeys = Collections.synchronizedList(new ArrayList<ByteBuffer>());
 
         long start = System.currentTimeMillis();
 
@@ -85,6 +110,166 @@ public class IntegrationTest {
                         log.error("Connection problem", ioe);
                         return;
                     }
+
+                    ByteBuffer key = ByteBuffer.allocate(ServerStorage.MD5_HASH_LENGTH);
+                    ByteBuffer chunk = ByteBuffer.allocate(ServerStorage.CHUNK_LENGTH);
+
+                    for (int j = 0; j < requestsNumber; j++) {
+                        r.nextBytes(key.array());
+                        r.nextBytes(chunk.array());
+
+                        try {
+                            client.push(key, chunk);
+                            if (!Arrays.equals(chunk.array(), client.pull(key).array())) {
+                                throw new IOException("Returned result is incorrect.");
+                            }
+                            if (!client.seek(key)) {
+                                throw new IOException("Can`t seek existed key.");
+                            }
+                            if (r.nextBoolean()) {
+                                if (!client.remove(key)) {
+                                    throw new IOException("OneNodeClient couldn`t remove chunk.");
+                                }
+                                if (client.pull(key) != null) {
+                                    throw new IOException("OneNodeClient returned previously removed chunk.");
+                                }
+                                if (client.seek(key)) {
+                                    throw new IOException("I have just found removed key.");
+                                }
+                                removedKeys.add(key);
+                            } else {
+                                chunksProcessed.incrementAndGet();
+                                storedChunks.add(new Pair(key, chunk));
+                            }
+
+                        } catch (IOException ex) {
+                            log.error("Oops!", ex);
+                            errorsCounter.incrementAndGet();
+                        }
+
+                        //if (j % 100 == 0)
+                        //    log.info("{} pull/push/?remove/pull request quads passed by {}.", j, this.hashCode());
+                        if (requestsPassed.incrementAndGet() % 1000 == 0) {
+                            log.info("{} pull/push/?remove/pull request quads passed.", requestsPassed.get());
+                        }
+
+                    }//for
+
+                    try {
+                        client.close();
+                    } catch (IOException ex) {
+                    }
+
+                    log.info("one of clients has just finished.");
+                    latch.countDown();
+
+                }
+            }.start();
+
+        }//while
+
+        latch.await();
+
+        log.info("All chunks were loaded for {} seconds.", (System.currentTimeMillis() - start) / 1000);
+
+        assertEquals(0, errorsCounter.get());
+
+        OneNodeClient client = OneNodeClient.newClient(host, port);
+        int chunksChecked = 0;
+        for (Pair pair : storedChunks) {
+            if (removedKeys.contains(pair.getKey())) {
+                continue;
+            }
+            assertTrue(client.seek(pair.getKey()));
+            assertTrue(Arrays.equals(pair.getValue().array(), client.pull(pair.getKey()).array()));
+            if (++chunksChecked % 10 == 0) {
+                log.info("{} chunks were checked.", requestsPassed.get());
+            }
+
+        }
+
+        assertTrue(chunksProcessed.get() * ServerStorage.WHOLE_CHUNK_WITH_META_LENGTH <= file.length());
+
+    }//simpleTest
+
+    @Ignore
+    @Test
+    public void clusterTest() throws IOException, InterruptedException {
+        String host = "127.0.0.1";
+        int port = 7620;
+
+        Topology topology = new Topology();
+
+        List<Byte> mask = Lists.newArrayList((byte) 0);
+        Set<Node> nodes = Sets.newHashSet(new Node(host, 7620));
+        topology.addKeyMask(mask, nodes);
+
+        mask = Lists.newArrayList((byte) 1);
+        nodes = Sets.newHashSet(new Node(host, 7621));
+        topology.addKeyMask(mask, nodes);
+
+        FileWriter writer = new FileWriter("topology.json");
+        new Gson().toJson(topology.getKeyMasks(), writer);
+        writer.close();
+
+        int serversCount = 2;
+
+        for (int i = 0; i < serversCount; i++) {
+
+            String fileName = String.format("test%s.bin", i);
+            File file = new File(fileName);
+
+            if (file.exists()) {
+                file.delete();
+            }
+            file.createNewFile();
+
+            ServerParams serverParams = new ServerParams();
+            serverParams.setStorageFile(file.getAbsolutePath());
+
+            serverParams.setInitialIndexSize(5000);
+
+            serverParams.setHost(host);
+            serverParams.setPort(port + i);
+
+            serverParams.setEncrypt(true);
+
+            serverParams.setServeAll(true);
+            serverParams.setNewKeysFile("new.keys");
+            serverParams.setOldKeysFile("old.keys");
+
+            serverParams.setMaxConnections(10);
+
+            writer = new FileWriter(String.format("server%s.json", i));
+            new Gson().toJson(serverParams, ServerParams.class, writer);
+            writer.close();
+
+            Server.main(String.format("server%s.json", i));
+            Thread.sleep(1000);
+
+        }//for initial
+
+        final ClusterClient client = ClusterClient.newClusterClient(topology);
+
+        for (Map.Entry<Node, OneNodeClient> entry : client.getClients().entrySet()) {
+            assertTrue(entry.getValue().isConnected());
+        }
+
+        final int clientsNumber = 10;
+        final int requestsNumber = 1000;
+        final AtomicInteger errorsCounter = new AtomicInteger(0);
+        final AtomicInteger requestsPassed = new AtomicInteger(0);
+        final AtomicInteger chunksStored = new AtomicInteger(0);
+        final CountDownLatch latch = new CountDownLatch(clientsNumber);
+        final ConcurrentMap<ByteBuffer, ByteBuffer> chunks = Maps.newConcurrentMap();
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < clientsNumber; i++) {
+            new Thread() {
+                @Override
+                public void run() {
+                    Random r = new Random();
 
                     ByteBuffer key = ByteBuffer.allocate(ServerStorage.MD5_HASH_LENGTH);
                     ByteBuffer chunk = ByteBuffer.allocate(ServerStorage.CHUNK_LENGTH);
@@ -123,6 +308,9 @@ public class IntegrationTest {
                             }
                         } else {
                             chunksStored.incrementAndGet();
+                            key.rewind();
+                            chunk.rewind();
+                            chunks.putIfAbsent(key, chunk);
                         }
 
                         //if (j % 100 == 0)
@@ -133,141 +321,31 @@ public class IntegrationTest {
 
                     }//for
 
-                    try {
-                        client.close();
-                    } catch (IOException ex) {
-                    }
-
                     log.info("one of clients has just finished.");
                     latch.countDown();
 
-                }
+                }//run
             }.start();
-
-        }//while
+        }
 
         latch.await();
 
         assertEquals(0, errorsCounter.get());
-        assertTrue(chunksStored.get() * ServerStorage.WHOLE_CHUNK_WITH_META_LENGTH <= file.length());
-
         log.info("Test was passed for {} seconds.", (System.currentTimeMillis() - start) / 1000);
 
-    }//simpleTest
-
-    @Ignore
-    @Test
-    public void clusterTest() throws IOException, InterruptedException {
-        File file = new File("test.bin");
-        if (file.exists()) {
-            file.delete();
+        for (ConcurrentMap.Entry<ByteBuffer, ByteBuffer> entry : chunks.entrySet()) {
+            entry.getKey().rewind();
+            log.info("{}", entry.getKey());
+            log.info("{}", client.pull(entry.getKey()));
+            assertTrue(Arrays.equals(entry.getValue().array(), client.pull(entry.getKey()).array()));
         }
-        file.createNewFile();
 
-        String host = "127.0.0.1";
-        int port = 7620;
-
-        Topology topology = new Topology();
-
-        for (int i = 0; i < 2; i++) {
-            ServerParams serverParams = new ServerParams();
-            serverParams.setStorageFile(String.format("test%s.bin", i));
-
-            serverParams.setInitialIndexSize(5000);
-
-            serverParams.setHost(host);
-            serverParams.setPort(port + i);
-
-            serverParams.setEncrypt(true);
-
-            serverParams.setServeAll(true);
-            serverParams.setNewKeysFile("new.keys");
-            serverParams.setOldKeysFile("old.keys");
-
-            serverParams.setMaxConnections(10);
-
-            FileWriter writer = new FileWriter("server.json");
-            new Gson().toJson(serverParams, ServerParams.class, writer);
-            writer.close();
-
-            Server.main();
-
-            Thread.sleep(1000);
-
-            OneNodeClient client = OneNodeClient.newClient(host, port + i);
-            assertTrue(client.isConnected());
-            client.close();
-        }
+        client.close();
 
         /*
-         final OneNodeClient client = OneNodeClient.newClient(host, port);
 
-         assertTrue(client.isConnected());
-
-         final int clientsNumber = 10;
-         final int requestsNumber = 1000;
-         final AtomicInteger errorsCounter = new AtomicInteger(0);
-         final AtomicInteger requestsPassed = new AtomicInteger(0);
-         final AtomicInteger chunksStored = new AtomicInteger(0);
-         final CountDownLatch latch = new CountDownLatch(clientsNumber);
-
-         long start = System.currentTimeMillis();
-
-         for (int i = 0; i < clientsNumber; i++) {
-         new Thread() {
-         @Override
-         public void run() {
-         ByteBuffer key = ByteBuffer.allocate(ServerStorage.MD5_HASH_LENGTH);
-         ByteBuffer chunk = ByteBuffer.allocate(ServerStorage.CHUNK_LENGTH);
-
-         for (int j = 0; j < requestsNumber; j++) {
-         r.nextBytes(key.array());
-         r.nextBytes(chunk.array());
-
-         try {
-         client.push(key, chunk);
-         if (!Arrays.equals(chunk.array(), client.pull(key).array()))
-         log.error("Returned result is incorrect.");
-         } catch (IOException ex) {
-         log.error("Oops!", ex);
-         errorsCounter.incrementAndGet();
-         }
-
-         if (r.nextBoolean()) {
-         try {
-         if (!client.remove(key))
-         throw new IOException("OneNodeClient couldn`t remove chunk.");
-         if (client.pull(key) != null)
-         throw new IOException("OneNodeClient returned previously removed chunk.");
-         } catch (IOException exception) {
-         errorsCounter.incrementAndGet();
-         log.error("Oops!", exception);
-         }
-         } else {
-         chunksStored.incrementAndGet();
-         }
-
-         if (requestsPassed.incrementAndGet() % 1000 == 0)
-         log.info("{} pull/push/?remove/pull request quads passed.", requestsPassed.get());
-
-         }//for
-
-         log.info("one of clients has just finished.");
-         latch.countDown();
-
-         }//run
-         }.start();
-
-         }//while
-
-         latch.await();
-
-         assertEquals(0, errorsCounter.get());
          assertTrue(chunksStored.get() * ServerStorage.WHOLE_CHUNK_WITH_META_LENGTH <= file.length());
 
-         log.info("Test was passed for {} seconds.", (System.currentTimeMillis() - start) / 1000);
-
-         client.close();
          */
     }//clusterTest
 
